ec78e1de5183a47892da0438a86056359c7aff5a
mabutamail <mabutamail@gmail.com>
2020-02-18 21:05:17 +0300

    add Variable.java

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte countProcessor = -128;
+        short sizeHardDisk = 32_767;
+        int rateProcessor = 2_000_000_000;
+        long price = 1_234_567_890_123_456_789L;
+        float rateVideoCard = 9.876_543_210F;
+        double rateMemory = 1.234_567_890D;
+        char prefixModel = 'I';
+        boolean isForGamers = true;
+
+        System.out.println(countProcessor);
+        System.out.println(sizeHardDisk);
+        System.out.println(rateProcessor);
+        System.out.println(price);
+        System.out.println(rateVideoCard);
+        System.out.println(rateMemory);
+        System.out.println(prefixModel);
+        System.out.println(isForGamers);
+
+    }
+}
