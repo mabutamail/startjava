cd05572b316c5e174a6b42446e98599c3f47868b
mabutamail <mabutamail@gmail.com>
2020-02-20 20:39:48 +0300

    add Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNum = 7;
+        int secondNum = 2;
+        char operation = '^';
+        double result = 0;
+        if (operation == '+') {
+            result = firstNum + secondNum;
+        } else if (operation == '-') {
+            result = firstNum - secondNum;
+        } else if (operation == '*') {
+            result = firstNum * secondNum;
+        } else if (operation == '/') {
+            if (secondNum == 0) {
+                System.out.println("ошибка: деление на ноль!");
+                return;
+            } else {
+                result = 1.0 * firstNum / secondNum;
+            }
+        } else if (operation == '%') {
+            result = firstNum % secondNum;
+        } else if (operation == '^') {
+            result = 1;
+            for (int i = 0; i < secondNum; i++) {
+                result *= firstNum;
+            }
+        }
+
+        System.out.println("результат операции равен: " + firstNum + operation + secondNum + " = " + result);
+    }
+}
