ca731c3b1e9b06e0fb1b0f12db26c40806f67a25
mabutamail <mabutamail@gmail.com>
2020-03-03 17:56:14 +0300

    implements CalculatorTest.java HW2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,35 +1,68 @@
 public class Calculator {
-    public static void main(String[] args) {
-        int firstNum = 7;
-        int secondNum = 0;
-        char operation = '^';
-        double result = 0;
-        if (operation == '+') {
-            result = firstNum + secondNum;
-        } else if (operation == '-') {
-            result = firstNum - secondNum;
-        } else if (operation == '*') {
-            result = firstNum * secondNum;
-        } else if (operation == '/') {
-            if (secondNum == 0) {
-                System.out.println("ошибка: деление на ноль!");
-                return;
-            } else {
-                result = 1.0 * firstNum / secondNum;
-            }
-        } else if (operation == '%') {
-            if (secondNum == 0) {
-                System.out.println("ошибка: деление на ноль!");
-                return;
-            }
-            result = firstNum % secondNum;
-        } else if (operation == '^') {
-            result = 1;
-            for (int i = 0; i < secondNum; i++) {
-                result *= firstNum;
-            }
+
+    private double result;
+    private int firstNum ;
+    private int secondNum;
+    private char operation;
+
+
+    public void setFirstNum(int firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public void setSecondNum(int secondNum) {
+        this.secondNum = secondNum;
+    }
+
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+
+    public double calculate() {
+
+        switch (operation) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+
+            case '-':
+                result = firstNum - secondNum;
+                break;
+
+            case '*':
+                result = firstNum * secondNum;
+                break;
+
+            case '/':
+                if (secondNum == 0) {
+                    System.out.println("ошибка: деление на ноль!");
+                    return 0;
+                } else {
+                    result = 1.0 * firstNum / secondNum;
+                }
+                break;
+
+            case '%':
+                if (secondNum == 0) {
+                    System.out.println("ошибка: деление на ноль!");
+                    return 0;
+                } else {
+                    result = 1.0 * firstNum % secondNum;
+                }
+                break;
+
+            case '^':
+                result = 1;
+                for (int i = 0; i < secondNum; i++) {
+                    result *= firstNum;
+                }
+                break;
+            default:
+                System.out.println("неопределенный знак математической операции");
+                break;
         }
 
-        System.out.println("результат операции равен: " + firstNum + operation + secondNum + " = " + result);
+        return result;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,65 +1,24 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
+
     public static void main(String[] args) {
-        int firstNum = 7;
-        int secondNum = 4;
-        char operation = '^';
-        double result = 0;
+        Scanner scan = new Scanner(System.in);
+        Calculator calc = new Calculator();
 
 
         System.out.print("Введите первое число: ");
-        Scanner scan = new Scanner(System.in);
-        firstNum = scan.nextInt();
+        int firstNum = scan.nextInt();
+        calc.setFirstNum(firstNum);
+
         System.out.print("Введите знак математической операции: ");
-        operation = (char) scan.next().charAt(0);
+        char operation = scan.next().charAt(0);
+        calc.setOperation(operation);
+
         System.out.print("Введите второе число: ");
-        secondNum = scan.nextInt();
+        int secondNum = scan.nextInt();
+        calc.setSecondNum(secondNum);
 
-
-        switch (operation) {
-            case '+':
-                result = firstNum + secondNum;
-                break;
-
-            case '-':
-                result = firstNum - secondNum;
-                break;
-
-            case '*':
-                result = firstNum * secondNum;
-                break;
-
-            case '/':
-                if (secondNum == 0) {
-                    System.out.println("ошибка: деление на ноль!");
-                    return;
-                } else {
-                    result = 1.0 * firstNum / secondNum;
-                }
-                break;
-
-            case '%':
-                if (secondNum == 0) {
-                    System.out.println("ошибка: деление на ноль!");
-                    return;
-                } else {
-                    result = 1.0 * firstNum % secondNum;
-                }
-                break;
-
-            case '^':
-                result = 1;
-                for (int i = 0; i < secondNum; i++) {
-                    result *= firstNum;
-                }
-                break;
-            default:
-                System.out.println("неопределенный знак математической операции");
-                break;
-        }
-
-
-        System.out.println("результат операции равен: " + firstNum + operation + secondNum + " = " + result);
+        System.out.println("результат операции равен: " + firstNum + operation + secondNum + " = " + calc.calculate());
     }
 }
