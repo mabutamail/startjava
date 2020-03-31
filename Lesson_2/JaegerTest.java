c5abc9eebe749f93b9ec2d8ee1a0fa13267e3369
mabutamail <mabutamail@gmail.com>
2020-03-31 13:41:45 +0300

    implement HW2

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,28 @@
+public class JaegerTest {
+    public static void main(String[] arr) {
+        Jaeger bracerPhoenix = new Jaeger();
+        Jaeger gipsyDanger = new Jaeger("Gipsy Danger", "Mark-3", "USA", 79.25f, 1.98f, 7, 8, 6);
+
+        bracerPhoenix.setModelName("Bracer Phoenix");
+        bracerPhoenix.setMark("Mark-5");
+        bracerPhoenix.setOrigin("USA");
+        bracerPhoenix.setHeight(70.7f);
+        bracerPhoenix.setWeight(2.1f);
+        bracerPhoenix.setSpeed(3);
+        bracerPhoenix.setStrength(8);
+        bracerPhoenix.setArmor(9);
+
+        if (bracerPhoenix.drift()) System.out.println("дрифт 89%");
+        bracerPhoenix.move();
+        System.out.println(bracerPhoenix.getModelName() + ": " + bracerPhoenix.scanKaiju());
+        bracerPhoenix.useVortexCannon();
+
+        if (gipsyDanger.drift()) System.out.println("дрифт 55%");
+        gipsyDanger.move();
+        System.out.println(gipsyDanger.getModelName() + ": " + gipsyDanger.scanKaiju());
+        gipsyDanger.useVortexCannon();
+
+        System.out.println(bracerPhoenix);
+        System.out.println(gipsyDanger);
+    }
+}
